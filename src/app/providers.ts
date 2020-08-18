972c547f947864799ab42440b82ac03fedfb665c
AMINEAALI <Amine.Aali@hotmail.com>
2020-08-18 18:01:37 +0200

    this

---
+++ src/app/providers.ts
@@ -0,0 +1,65 @@
+export class Providers {
+
+    public providers: any = {
+        "aws": {
+          "displayName": "AWS",
+          "fullDisplayName": "Amazon Web Services",
+          "icon": "../../assets/images/AWS.png",
+          "smallIcon": "../../assets/images/AWS-small.png",
+          "color": "#f6ac33",
+          "description": {
+            "EN": "AWS is the cloud platform of Amazon"
+          },
+          "codeValue": "aws",
+          "display": true
+        },
+        "azure": {
+          "displayName": "Azure",
+          "fullDisplayName": "Microsoft Azure",
+          "icon": "../../assets/images/Azure.png",
+          "smallIcon": "../../assets/images/Azure-small.png",
+          "color": "#0072c6",
+          "description": {
+            "EN": "Azure is the cloud platform of Microsoft"
+          },
+          "codeValue": "azure",
+          "display": true
+        },
+        "google": {
+          "displayName": "Google",
+          "fullDisplayName": "Google Cloud Platform",
+          "icon": "../../assets/images/GCP.svg",
+          "smallIcon": "../../assets/images/GCP-small.png",
+          "color": "#34A853",
+          "description": {
+            "EN": "Google Cloud Platform"
+          },
+          "codeValue": "google",
+          "display": true
+        },
+        "ovh": {
+          "displayName": "OVHcloud",
+          "fullDisplayName": "OVHcloud",
+          "icon": "../../assets/images/OVH.png",
+          "smallIcon": "../../assets/images/OVH-small.png",
+          "color": "#000E9C",
+          "description": {
+            "EN": "OVHcloud"
+          },
+          "codeValue": "ovh",
+          "display": true
+        },
+        "onprem": {
+          "displayName": "On premise",
+          "fullDisplayName": "On premise",
+          "icon": "../../assets/images/privateDC.png",
+          "smallIcon": "../../assets/images/privateDC-small.png",
+          "color": "#A5A5A5",
+          "description": {
+            "EN": "On premise datacenter"
+          },
+          "codeValue": "onprem",
+          "display": true
+        }
+      }
+}
